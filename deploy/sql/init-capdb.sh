15cd07b4298727e6b3894b66bfe8bdf38d9459cc
Avan Suinesiaputra <avan.sp@gmail.com>
2016-04-07 15:38:20 +1200

    UPD: Add admin user creation

--- deploy/sql/init-capdb.sh
+++ deploy/sql/init-capdb.sh
@@ -22,3 +22,25 @@ mysql -uroot -p < create-capdb.mysql
 echo 'Creating CAPUSERS database...'
 echo 'Please enter root password to mysql'
 mysql -uroot -p < create-capusers.mysql
+
+# CREATE ADMIN PASSWORD
+echo 'Creating ADMIN password'
+echo 'Please enter again root password to mysql'
+mysql -uroot -p << EOFSQL
+DELETE FROM CAPUSERS.Users WHERE username='ADMIN';
+
+INSERT INTO CAPUSERS.Users
+    (username,
+     name,
+     passwd,
+     userdescription,
+     lastlogin,
+     lastpasswordchange)
+VALUES
+    ('ADMIN',
+     'CAP Administrator',
+     PASSWORD("'${adminpass}'"),
+     'Super user',
+     CURTIME(),
+     CURTIME());
+EOFSQL
