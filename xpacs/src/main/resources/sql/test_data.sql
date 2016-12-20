22d03533bf24bb85b63adadcd6c92d682e8d2fd0
Avan Suinesiaputra <avan.sp@gmail.com>
2016-12-20 13:28:21 +1300

    completed initialization SQL script

--- xpacs/src/main/resources/sql/test_data.sql
+++ xpacs/src/main/resources/sql/test_data.sql
@@ -19,3 +19,31 @@ VALUES
 	('PUBLIC', 'Caucasian', 'F', 'PUB00021', 'Infarction');
 
 /* ADD CLINICAL_NOTE */
+INSERT INTO CLINICAL_NOTE (assessment_date, age, notes, diagnosis, height, weight, patient_id)
+VALUES
+	('2007-07-11', 30.5, 'first scan', NULL, 101.2, 48.0, (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0005')),
+	('2002-09-10', NULL, 'routine follow-up check', NULL, NULL, NULL, (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD00012')),
+	('2008-02-23', 30.5, 'second scan', NULL, 101.2, 48.0, (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0005')),
+	('2010-10-06', 20, 'nothing major', 'patient had unexplained symptoms', 98, 77.6, (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='PUB00021')),
+	('1998-02-08', 19, 'recruitment', 'this is initial setup', 98.0, 55, (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='PUB00021')),
+	('2011-06-20', 30.5, 'follow-up', NULL, NULL, NULL, (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD00011'));
+
+/* ADD AUX_FILE */
+INSERT INTO AUX_FILE (created_date, filename, uri, description,  patient_id)
+VALUES
+	('2015-10-20', 'report1.pdf', '/2015/10/', 'clinician report of a patient', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0005')),
+	('2015-10-21', 'report2.pdf', '/2015/10/', 'clinician report of the same patient', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0005')),
+	('2011-09-11', 'somesheet.csv', '/2011/09/', 'diagnostic table', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0002')),
+	('2014-09-10', 'snapshot.jpg', '/2014/09/', 'image snapshots', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='PUB00021'));
+
+/* ADD CAP_MODEL */
+INSERT INTO CAP_MODEL (created_date, name, patient_id)
+VALUES
+	('2001-11-09', 'cap_chd0001_ao', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0001')),
+	('2001-11-10', 'cap_chd0005_ao', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0005')),
+	('2001-07-11', 'cap_chd0002_ao', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD0002')),
+	('2011-11-02', 'cap_pub00100_ao', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='PUB00100')),
+	('2001-10-01', 'cap_pub00008_ao', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='PUB00008')),
+	('2013-06-25', 'cap_chd00011_ao', (SELECT patient_id FROM PATIENT_INFO WHERE patient_id='CHD00011'));
+
+/* BASELINE_DIAGNOSIS IS INITIALIZED AS EMPTY */
